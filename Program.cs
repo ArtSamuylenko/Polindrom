b4eea4e2fe396a9a6fd4389bc55d9fd932e65825
ArtSamuylenko <89385274775@mail.ru>
2021-12-11 21:42:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string a = Console.ReadLine();
+        string b = a.Replace(" ", "");
+        Console.WriteLine($"{b}");
+        bool opred = true;
+        for (int i = 0; i < b.Length; i++)
+        {
+            if (b[i] == b[b.Length - 1 - i])
+            {
+                opred = true;
+            }
+            else
+            {
+                opred = false;
+            }
+            if (!opred)
+            {
+                Console.WriteLine("Это не полиндром");
+                break;
+            }
+        }
+        if (opred)
+        {
+            Console.WriteLine("Это полиндром");
+        }
+    }
+}
